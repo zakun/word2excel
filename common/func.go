bf0e62b486690e8abbeca28f93183805205cc7c0
qizk <qizk@mail.open.com.cn>
2024-06-06 14:04:16 +0800

    代码优化

--- common/func.go
+++ common/func.go
@@ -2,7 +2,7 @@
  * @Author: qizk qizk@mail.open.com.cn
  * @Date: 2024-05-14 11:00:37
  * @LastEditors: qizk qizk@mail.open.com.cn
- * @LastEditTime: 2024-05-30 17:21:37
+ * @LastEditTime: 2024-06-06 13:58:56
  * @FilePath: \word2excel\common\func.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -30,6 +30,10 @@ func PC(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func Sprintf(format string, a ...any) string {
+	return fmt.Sprintf(format, a...)
 }
 
 func IsExistDir(path string) bool {
--- main.go
+++ main.go
@@ -2,7 +2,7 @@
  * @Author: qizk qizk@mail.open.com.cn
  * @Date: 2022-09-06 13:44:45
  * @LastEditors: qizk qizk@mail.open.com.cn
- * @LastEditTime: 2024-06-06 13:53:52
+ * @LastEditTime: 2024-06-06 14:00:20
  * @FilePath: \helloworld\hello.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,7 +10,6 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	"io/fs"
 
 	"path/filepath"
@@ -38,7 +37,7 @@ func main() {
 			extname := filepath.Ext(name)
 			if extname == ".docx" {
 				fileNo++
-				logger.Info(fmt.Sprintf("Word 文件: %v# %v", fileNo, name))
+				logger.Info(common.Sprintf("Word 文件: %v# %v", fileNo, name))
 				arrQuestion := word.ParseContent(name)
 
 				// common.PF("questions: %v", arrQuestion)
