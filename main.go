69cb969628d4159354b81834ecd97fb55b775c21
qizk <qizk@mail.open.com.cn>
2024-06-28 14:00:02 +0800

    代码优化

--- main.go
+++ main.go
@@ -2,7 +2,7 @@
  * @Author: qizk qizk@mail.open.com.cn
  * @Date: 2022-09-06 13:44:45
  * @LastEditors: qizk qizk@mail.open.com.cn
- * @LastEditTime: 2024-06-26 15:05:42
+ * @LastEditTime: 2024-06-28 13:58:57
  * @FilePath: \helloworld\hello.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -36,6 +36,10 @@ func main() {
 	defer func() {
 		logger.Close()
 	}()
+
+	if maxN <= 0 {
+		common.Throw_panic(errors.New("并发数至少大于0"))
+	}
 
 	if template == "" {
 		common.Throw_panic(errors.New("请输入待解析的模板类型"))
